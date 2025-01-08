c1a74bc2b3e120acc30b2e9fdc5a1a1cbf8573ca
aaron cheese skibidi rizz <126525235+aaron21913@users.noreply.github.com>
2025-01-08 19:04:18 +0000

    rizz panther

--- Dog.java
+++ Dog.java
@@ -5,12 +5,24 @@ public class Dog
   private static int numDogs = 0;
 
   // Non-default Dog constructor
+  public Dog(String name)
+  {
+    this.name = name;
 
+    numDogs++;
+    this.ID = numDogs * numDogs;
+  }
   // Default Dog constructor
 
   // Setters - instance variables only
-
+  public voi setName(String iName)
+  {
+    name = iName;
+  }
   // Getters - instance variables only
-
+  public String getName()
+  {
+    return name;
+  }
   // method getNumDogs
 }
